eccd1e93aacb54303849e5135444dd11e8a8f447
magnusvg <magnus-kil@live.no>
2021-01-26 17:26:15 +0100

    Pushmaster Flex

---
+++ PyIntegration/buildprg.py
@@ -0,0 +1,4 @@
+import os
+
+# Build Monkey C program through shell
+os.system("monkeyc -d fenix6 -f C:/Users/magnu/eclipse-workspace/SeveralDataFields/monkey.jungle -o C:/Users/magnu/eclipse-workspace/SeveralDataFields/bin/SeveralDataFields.prg -y C:/Users/magnu/keys/developer_key")
