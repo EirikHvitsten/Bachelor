0f75b980a4848b56efaf8796168662c6cc822eac
EirikHvitsten <54394995+EirikHvitsten@users.noreply.github.com>
2021-05-14 22:05:24 +0200

    cleanup

--- søppelkasse/PyIntegration/buildprg.py
+++
@@ -1,4 +0,0 @@
-import os
-
-# Build Monkey C program through shell
-os.system("monkeyc -d fenix6 -f C:/Users/magnu/eclipse-workspace/SeveralDataFields/monkey.jungle -o C:/Users/magnu/eclipse-workspace/SeveralDataFields/bin/SeveralDataFields.prg -y C:/Users/magnu/keys/developer_key")
--- søppelkasse/PyIntegration/run.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Build XML and PRG
-os.system("c:/Users/magnu/OneDrive/Documents/Bachelor/PyIntegration/buildxml.py")
-os.system("c:/Users/magnu/OneDrive/Documents/Bachelor/PyIntegration/buildprg.py")
-
-# Run simulator
-os.system("connectiq")
-os.system("monkeydo C:/Users/magnu/eclipse-workspace/SeveralDataFields/bin/SeveralDataFields.prg fenix6")
